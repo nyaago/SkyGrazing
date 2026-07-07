efa3e79ba2b4ae07a3b3efdd75f6f2f202652c39
nyaago <nyaago69@gmail.com>
2026-07-07 23:28:31 +0900

    add AuthorFeedRequest

---
+++ SkyGrazing/ApiClients/BskyAuthorFeedRequest.swift
@@ -0,0 +1,46 @@
+//
+//  BskyAuthorFeedRequest.swift
+//  SkyGrazing
+//
+//  Created by nyaago on 2026/07/07.
+//
+
+// https://docs.bsky.app/docs/api/app-bsky-feed-get-author-feed
+
+import Foundation
+
+struct BskyAuthorFeedRequest: BskyRequestable {
+    typealias Response = BskyTimeline
+
+    let actor: String
+    let limit: Int?
+    let cursor: String?
+    let filter: String?
+
+    init(actor: String, limit: Int? = nil, cursor: String? = nil, filter: String? = nil) {
+        self.actor = actor
+        self.limit = limit
+        self.cursor = cursor
+        self.filter = filter
+    }
+
+    func buildQueryItems() -> [URLQueryItem] {
+        var items: [URLQueryItem] = [
+            URLQueryItem(name: "actor", value: actor)
+        ]
+        if let limit {
+            items.append(URLQueryItem(name: "limit", value: String(limit)))
+        }
+        if let cursor {
+            items.append(URLQueryItem(name: "cursor", value: cursor))
+        }
+        if let filter {
+            items.append(URLQueryItem(name: "filter", value: filter))
+        }
+        return items
+    }
+
+    func endPoint() -> String {
+        "app.bsky.feed.getAuthorFeed"
+    }
+}
