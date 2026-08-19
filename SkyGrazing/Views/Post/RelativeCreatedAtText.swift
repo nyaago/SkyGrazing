85a56217017fd78ed938c6cf22f96ab53248b50d
nyaago <nyaago69@gmail.com>
2026-08-19 22:53:09 +0900

    Add ISO8601DateParser & RelativeDateFormatter Utils

--- SkyGrazing/Views/Post/RelativeCreatedAtText.swift
+++ SkyGrazing/Views/Post/RelativeCreatedAtText.swift
@@ -11,40 +11,14 @@ struct RelativeCreatedAtText: View {
     let createdAt: String?
 
     var body: some View {
-        Text(formatCreatedAt() ?? "").modifier(CaptionModifier())
+        Text(formattedText).modifier(CaptionModifier())
     }
-    
-    private func formatCreatedAt() -> String? {
+
+    private var formattedText: String {
         if let createdAt {
-            return formatDate(dateString: createdAt)
+            return RelativeDateFormatter.string(from: createdAt) ?? ""
         }
-        return nil
-    }
-
-    private func formatDate(dateString: String) -> String? {
-        guard let date = dateFrom(dateString: dateString)
-        else { return nil }
-        return format(date: date)
-    }
-
-    private func format(date: Date) -> String {
-        let formatter = RelativeDateTimeFormatter()
-        formatter.unitsStyle = .abbreviated
-        formatter.dateTimeStyle = .numeric
-        formatter.unitsStyle = .abbreviated
-        return formatter.localizedString(for: date, relativeTo: Date())
-    }
-    
-    private func dateFrom(dateString: String) -> Date? {
-        let formatter = ISO8601DateFormatter()
-        // 小数秒ありでまず試行
-        formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
-        if let date = formatter.date(from: dateString) {
-            return date
-        }
-        // 小数秒なしで再試行
-        formatter.formatOptions = [.withInternetDateTime]
-        return formatter.date(from: dateString)
+        return ""
     }
 }
 
